ffee3f22517eb813b6a58c451cd3765f80e995e6
rlazani <rlazani@hotmail.com>
2025-10-10 21:46:31 -0300

    iniciaizacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MenuComponent, PrincipalComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'guia-horta';
